5e04b287f825467b274b458eebe17a03e95025fd
Facundo Flores <flores.facundogabriel@exa.unsa.edu.ar>
2017-09-08 09:54:38 -0300

    Add debounceTime for improving performance Our previous app was sending too many requests With debounceTime we need to wait (xxx)ms without receiving data to send the next request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from './app.service';
 import { Post } from './post.model';
 
 import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-root',
@@ -23,17 +25,22 @@ import { Subject } from 'rxjs/Subject';
   `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   data: Array<Post> = [];
 
   // This variable is used to be output and input from the event
   keyword$ = new Subject();
 
-  constructor(private searchService: SearchService) {
-    this.keyword$.subscribe(keyword => this.search(keyword))
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit() {
+    this.keyword$
+      .debounceTime(500)
+      .subscribe(keyword => this.search(keyword));
   }
 
   search(keyword): void {
-    this.searchService.search(keyword).subscribe(data => this.data = data);
+    this.searchService.search(keyword)
+      .subscribe(data => this.data = data);
   }
 }
